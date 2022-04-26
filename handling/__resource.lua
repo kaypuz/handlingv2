f32b8eb666e6c0e045f4c7c7bffdfaa78a6ef355
Can Demircan <53577964+kaypuz@users.noreply.github.com>
2022-04-26 23:02:21 +0300

    main

---
+++ handling/__resource.lua
@@ -0,0 +1,25 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+data_file 'HANDLING_FILE' 'compacts.meta'
+data_file 'HANDLING_FILE' 'coupes.meta'
+data_file 'HANDLING_FILE' 'super.meta'
+data_file 'HANDLING_FILE' 'sports.meta'
+data_file 'HANDLING_FILE' 'sedans.meta'
+data_file 'HANDLING_FILE' 'suvs.meta'
+data_file 'HANDLING_FILE' 'muscle.meta'
+data_file 'HANDLING_FILE' 'mpsum.meta'
+data_file 'HANDLING_FILE' 'mpheist3.meta'
+data_file 'HANDLING_FILE' 'mpvinewood.meta'
+
+files {
+	'compacts.meta',
+	'super.meta',
+	'sports.meta',
+	'sedans.meta',
+	'suvs.meta',
+	'coupes.meta',
+	'mpsum.meta',
+	'muscle.meta',
+	'mpvinewood.meta',
+	'mpheist3.meta'
+}
